a5f7e269ef06ac1df902db1ead1edb0fd3fab90a
Arjun Mishra <arjunmishra31204@gmail.com>
2024-06-20 14:04:37 -0700

    removed beam background and possible fix

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 import { motion } from "framer-motion";
-import { BackgroundBeams } from "./ui/background-beams";
 import Link from "next/link";
 import Image from "next/image";
 import { FlipWords } from "./ui/flip-words";
@@ -46,9 +45,6 @@ export function Intro() {
             </div>
           {/* </div> */}
       </motion.div>
-      
-
-      <BackgroundBeams className="h-screen" />py-20 flex items-center justify-center
     </div>
   );
 };
--- components/experiences.tsx
+++ components/experiences.tsx
@@ -7,7 +7,7 @@ export function Experiences() {
   return (
     <div className="p-10 grid grid-cols-1 lg:grid-cols-3 gap-4 max-w-7xl mx-auto w-full">
       <WobbleCard
-        containerClassName="col-span-1 lg:col-span-2 h-full bg-blue-900  min-h-[500px] lg:min-h-[300px]"
+        containerClassName="col-span-1 lg:col-span-2 h-full bg-blue-900  min-h-[900px] lg:min-h-[900px]"
         className=""
       >
         <Image
@@ -47,7 +47,7 @@ export function Experiences() {
           className="absolute -right-4 lg:-right-[40%] grayscale filter -bottom-10 object-contain rounded-2xl"
         /> */}
       </WobbleCard>
-      <WobbleCard containerClassName="col-span-1 row-span-1 min-h-[300px]">
+      <WobbleCard containerClassName="col-span-1 row-span-1 min-h-[800px]">
         <h2 className="max-w-80  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
           Software Developer Intern| MorningStar Technologies| Lucknow, UP,
           India
@@ -73,7 +73,7 @@ export function Experiences() {
           className="relative -right-[70%] -bottom-10 object-contain rounded-xl"
         />
       </WobbleCard>
-      <WobbleCard containerClassName="col-span-1 lg:col-span-2 bg-pink-800 min-h-[400px] lg:min-h-[400px] xl:min-h-[300px]">
+      <WobbleCard containerClassName="col-span-1 lg:col-span-2 bg-pink-800 min-h-[800px] lg:min-h-[800px] xl:min-h-[700px]">
         <div className="max-w-sm pb-8">
           <h2 className="max-w-sm md:max-w-lg  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
             Flutter Developer Intern| A2D Innovations Pvt Ltd| Lucknow, UP,
